bac3f0b91c8bf3518c2dede2afe5b5d03f8bce5f
Soyeon <isylsy166@gmail.com>
2026-02-24 14:50:00 +0900

    [chore] common 모듈 추가 및 의존성 설정변경

---
+++ mopl-common/src/test/java/mopl/common/MoplCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package mopl.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoplCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
